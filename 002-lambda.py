ddfc8f77ec3d9ad00dd6aed32ef072a4d7aefca0
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-12-08 15:31:01 -0800

    Learning Lambda

---
+++ 002-lambda.py
@@ -0,0 +1,14 @@
+# Lambda also known as Anonymous function
+# Lambda automaticcally returns the result of an Expression
+# It is basically an affiliate for Short Hand Code
+# ----------------------------------------
+
+
+def add_10(num):
+  return num + 10
+
+print(add_10(5))
+
+add_10_ = lambda num : num + 10
+
+print(add_10_(5))
